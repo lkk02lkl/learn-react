28f476daf12ce4e323b85e3937e70500f099936c
unknown <anhelinapavliuk@gmail.com>
2023-12-29 17:21:39 +0100

    react

--- learn/src/Content.js
+++ learn/src/Content.js
@@ -1,36 +1,7 @@
-import { useState } from "react";
+
 import { FaTrashAlt } from "react-icons/fa";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item));
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
+const Content = ({ items, handleCheck, handleDelete}) => {
 
   return (
     <main>
--- learn/src/Header.js
+++ learn/src/Header.js
@@ -1,9 +1,13 @@
-const Header = () => {
+const Header = ({title}) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+
+Header.defaulProps = {
+  title: "Default Title"
 };
 
 export default Header;
