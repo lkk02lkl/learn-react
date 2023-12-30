97304422684edfa80e08099de9412f7f29161f59
unknown <anhelinapavliuk@gmail.com>
2023-12-30 14:25:38 +0100

    react

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,18 @@
-
+import Square from "./Square";
+import Input from "./Input";
+import { useState } from 'react';
 
 function App() {
+  const [colorValue, setColorValue] = useState('');
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Square 
+        colorValue={colorValue}
+      />
+      <Input 
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+      />
     </div>
   );
 }
--- project/src/Square.js
+++ project/src/Square.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Square = () => {
+const Square = ({ colorValue }) => {
   return (
-    <div>Square</div>
+    <section
+      className='square'
+      style={{ backgroundColor: colorValue }}
+    >
+      <p>{colorValue ? colorValue : "Empty Value"}</p>
+
+    </section>
   )
+}
+
+Square.defaultProps = {
+  colorValue: "Empty Color Value"
 }
 
 export default Square
