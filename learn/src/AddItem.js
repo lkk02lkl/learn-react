c0ef3d41a62c6ae452fad5ea1438a76f5d2b4032
unknown <anhelinapavliuk@gmail.com>
2023-12-30 13:07:25 +0100

    react

---
+++ learn/src/AddItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+
+const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
+  return (
+    <form className='addForm' onSubmit={handleSubmit}>
+      <label htmlFor='addItem'>Add Item</label>
+      <input
+        autoFocus
+        id='addItem'
+        type='text'
+        placeholder='Add Item'
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button
+        type='submit'
+        aria-label='Add Item'
+      >
+        <FaPlus />
+      </button>
+    </form>
+    
+  )
+}
+
+export default AddItem
--- learn/src/LineItem.js
+++ learn/src/LineItem.js
@@ -3,7 +3,7 @@ import { FaTrashAlt } from "react-icons/fa";
 
 const LineItem = ({ item, handleCheck, handleDelete }) => {
   return (
-    <li className="item" key={item.id}>
+    <li className="item">
       <input 
         type="checkbox" 
         id={`checkbox-${item.id}`} 
