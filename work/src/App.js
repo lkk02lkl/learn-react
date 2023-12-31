0d55db9023b0c9d0cee7d5829dc0433edac81704
unknown <anhelinapavliuk@gmail.com>
2023-12-31 15:34:20 +0100

    react

--- work/src/App.js
+++ work/src/App.js
@@ -1,16 +1,14 @@
-import { useState, useEffect} from 'react';
-import Form from './Form';
-import List from './List';
+import { useState, useEffect } from "react";
+import Form from "./Form";
+import List from "./List";
+import Table from "./Table";
 
 function App() {
-
   const API_URL = "https://jsonplaceholder.typicode.com/";
-
-  const [reqType, setReqType] = useState('users');
+  const [reqType, setReqType] = useState("users");
   const [items, setItems] = useState([]);
-  
-  useEffect(() => {
 
+  useEffect(() => {
     const fetchItems = async () => {
       try {
         const response = await fetch(`${API_URL}${reqType}`);
@@ -19,21 +17,16 @@ function App() {
       } catch (err) {
         console.log(err);
       }
-    }
+    };
 
     fetchItems();
-
   }, [reqType]);
-
-
 
   return (
     <div className="App">
-      <Form 
-        reqType={reqType}
-        setReqType={setReqType}
-      />
-      <List items={items}/>
+      <Form reqType={reqType} setReqType={setReqType} />
+      {/* <List items={items} /> */}
+      <Table items={items} />
     </div>
   );
 }
