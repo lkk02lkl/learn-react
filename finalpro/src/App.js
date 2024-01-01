aa396c9d3a45c4e23d7b539aff2b86849a14abe5
unknown <anhelinapavliuk@gmail.com>
2024-01-01 14:11:03 +0100

    react

--- finalpro/src/App.js
+++ finalpro/src/App.js
@@ -1,24 +1,26 @@
-import Header from './Header'; 
-import Nav from './Nav'; 
-import Footer from './Footer'; 
-import Home from './Home'; 
-import NewPost from './NewPost'; 
-import PostPage from './PostPage'; 
-import About from './About'; 
-import Missing from './Missing'; 
-import { Route, Switch, useHistory } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
-      <Home />
-      <NewPost />
-      <PostPage />
-      <About />
-      <Missing />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
       <Footer />
     </div>
   );
--- finalpro/src/index.js
+++ finalpro/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Router>
-      <Route path='/' component={App} />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </Router>
-  </React.StrictMode>,
+  </StrictMode>,
 );
 
