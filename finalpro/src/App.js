8e7849f2caea71c472ee3dd2f9e76541b0bdd5c3
unknown <anhelinapavliuk@gmail.com>
2024-01-01 13:35:16 +0100

    react

---
+++ finalpro/src/App.js
@@ -0,0 +1,27 @@
+import Header from './Header'; 
+import Nav from './Nav'; 
+import Footer from './Footer'; 
+import Home from './Home'; 
+import NewPost from './NewPost'; 
+import PostPage from './PostPage'; 
+import About from './About'; 
+import Missing from './Missing'; 
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
