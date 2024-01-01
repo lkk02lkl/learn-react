fb012139ca5b1d2b6e1f3c330a5a70e999d01b20
unknown <anhelinapavliuk@gmail.com>
2024-01-01 18:12:38 +0100

    react

--- finalpro/src/App.js
+++ finalpro/src/App.js
@@ -6,8 +6,9 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Routes, useHistory } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { format } from 'date-fns';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -38,9 +39,22 @@ function App() {
   ]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title:postTitle, datetime, body: postBody};
+    const allPosts = { ...posts, newPost};
+  }
   
   const handleDelete = (id) => {
-    
+    const postsList = posts.filter(post => post.id !== id);
+    setPosts(postsList);
+    navigate('/');
   }
 
   return (
@@ -49,7 +63,13 @@ function App() {
       <Nav search={search} setSearch={setSearch}/>
       <Routes>
         <Route exact path="/" element={<Home posts={posts} />} />
-        <Route exact path="/post" element={<NewPost />} />
+        <Route exact path="/post" element={<NewPost 
+          handleSubmit={handleSubmit}
+          postBody={postBody} 
+          postTitle={postTitle} 
+          setPostTitle={setPostTitle} 
+          setPostBody={setPostBody} 
+        />} />
         <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Missing />} />
--- finalpro/src/NewPost.js
+++ finalpro/src/NewPost.js
@@ -1,11 +1,29 @@
-import React from "react";
+const NewPost = ({
+    handleSubmit, postTitle, setPostTitle, postBody, setPostBody
+}) => {
+    return (
+        <main className="NewPost">
+            <h2>New Post</h2>
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+                <input
+                    id="postTitle"
+                    type="text"
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle(e.target.value)}
+                />
+                <label htmlFor="postBody">Post:</label>
+                <textarea
+                    id="postBody"
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+    )
+}
 
-const NewPost = () => {
-  return (
-    <main>
-      <h1>NewPost</h1>
-    </main>
-  );
-};
-
-export default NewPost;
+export default NewPost
