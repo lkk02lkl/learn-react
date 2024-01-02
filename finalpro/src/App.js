dbcf9dd0f13c2bb617106b0757fb0289d96c6123
unknown <anhelinapavliuk@gmail.com>
2024-01-02 14:36:50 +0100

    react

--- finalpro/src/App.js
+++ finalpro/src/App.js
@@ -49,16 +49,22 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title:postTitle, datetime, body: postBody};
-    const allPosts = [ ...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostTitle('');
-    navigate('/');
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostTitle("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+    
   }
   
   const handleDelete = (id) => {
