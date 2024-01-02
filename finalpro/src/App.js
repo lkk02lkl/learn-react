f911beb4ba5385f6e1c8a91e3ff383609c6c0842
unknown <anhelinapavliuk@gmail.com>
2024-01-02 14:56:01 +0100

    react

--- finalpro/src/App.js
+++ finalpro/src/App.js
@@ -17,6 +17,8 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -66,11 +68,31 @@ function App() {
     }
     
   }
+
+  const handleEdit = async (id) =>{
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+      setEditTitle();
+      setPostBody();
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
   
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    navigate('/');
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`)
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+    
   }
 
   return (
---
+++ finalpro/src/EditPost.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const EditPost = ({
+  posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle
+}) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+
+  useEffect(() => {
+    if (post) {
+      setEditTitle(post.title);
+      setEditBody(post.body);
+    }
+  }, [post, setEditTitle, setEditBody]);
+
+  return (
+    <main className="NewPost">
+            <h2>New Post</h2>
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+                <input
+                    id="postTitle"
+                    type="text"
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle(e.target.value)}
+                />
+                <label htmlFor="postBody">Post:</label>
+                <textarea
+                    id="postBody"
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+  )
+}
+
+export default EditPost
