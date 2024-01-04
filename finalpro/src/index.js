95cf482de439ae411012d3af2170be531093a454
unknown <anhelinapavliuk@gmail.com>
2024-01-04 21:46:55 +0100

    react

--- finalpro/src/index.js
+++ finalpro/src/index.js
@@ -3,15 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import store from './store';
+import { StoreProvider } from "easy-peasy";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
   </StrictMode>,
 );
 
